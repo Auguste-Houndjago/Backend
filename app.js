e739eb1574996ace1da7442c5cfd1f94dd7715bb
Auguste-Houndjago <augustehoundjago@gmail.com>
2024-10-20 23:15:21 +0000

    feat: first add git

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+console.log(text)
+
+var text= "et moi"    
+
+
+
+console.log("ma femme",text)
+
+
+console.log(b)
+
+var b="ta tete pour le moment"
+
+
+function testVar() {
+    console.log(a); // undefined
+    var a = 5;
+    console.log(a); // 5
+}
+
+testVar()
